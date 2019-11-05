4d63d818c06cf42e2b7dfbe5efadeee9160941ce
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-05 20:26:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask , render_template
 from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
-
 
 status=0
 
