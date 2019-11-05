43f97171b300456d6e3896c6637053876d430dbe
Alihan Tutuk <reyizz43@gmail.com>
2019-11-05 15:09:27 +0300

    Changing layout.html and configure the app.py

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 from flask import Flask , render_template
-app = Flask(__name__)
+from forms import RegistrationForm,LoginForm
+app = Flask(_name_)
 
-x = [1,2,3,4,5]
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+
+
+status=0
 
 @app.route('/')
-@app.route('/home')
+@app.route('/Home')
 def homepage():
-    return render_template('home.html',posts = x)
+    global status
+    return render_template('home.html',Status =status)
 
-@app.route('/login')
-def loginpage():
-    return render_template('home.html', title = "Login Page")
+@app.route('/SignIn')
+def sign_in_page():
+    global status
+    status= 0
+    form = LoginForm()
+    return render_template('home.html',Status =status,title = "Login Page", form=form)
+
+@app.route('/SignUp')
+def sign_up_page():
+    global status
+    status= 0
+    return render_template('register.html',Status=status,title = "Register Page",form= form )
+
+
+
+@app.route('/Profile')
+def profile_page():
+    global status
+    status=1
+    return render_template('home.html',Status=status)
+
+@app.route('/Bag')
+def bag_page():
+    global status
+    return render_template('home.html',Status =status)
    
 
 
