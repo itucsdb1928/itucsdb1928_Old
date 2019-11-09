c72802486370533818b68e6bfe32feda7c9f77e7
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-09 22:11:21 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -6,6 +6,42 @@ import psycopg2 as dbapi2
 
 INIT_STATEMENTS = [
     """
+    CREATE TABLE Author( 
+                      AuthorID SERIAL PRIMARY KEY , 
+                      name VARCHAR(30), 
+                      surname VARCHAR(30),
+                      birthDate TIME , 
+                      numberOfbooks INTEGER,
+                      country VARCHAR(20)
+                     );
+    CREATE TABLE BookRewiev( 
+                      BookRewievID SERIAL PRIMARY KEY ,
+                      review INTEGER,
+                      PostDate TIME,
+                      UserRating INTEGER,
+                      lostEdit VARCHAR(50),
+                      orginalText VARCHAR(100) 
+                     );
+                     
+    CREATE TABLE Publisher( 
+                      PublisherID SERIAL PRIMARY KEY , 
+                      name VARCHAR(40), 
+                      adress VARCHAR(50),
+                      numberOfbooks INTEGER , 
+                      establishmentDate TIME ,
+                      logo VARBINARY(8000)
+                     );
+                     
+    CREATE TABLE Books( 
+                      BookID SERIAL PRIMARY KEY , 
+                      Title VARCHAR(20),
+                      PageNum INTEGER,
+                      BookRewiev INTEGER REFERENCES BookRewiev (BookRewievID) , 
+                      PublisherID INTEGER REFERENCES Publisher (PublisherID), 
+                      AuthorID INTEGER REFERENCES Author (AuthorID) 
+                      cover VARBINARY(8000)
+                     );  
+                                          
     CREATE TABLE UserContent( 
                       UserContentID SERIAL PRIMARY KEY , 
                       Vote INTEGER, 
@@ -26,43 +62,7 @@ INIT_STATEMENTS = [
                       password VARCHAR (20) UNIQUE NOT NULL,
                       isAdmin INTEGER
                      );
-                     
-    CREATE TABLE Publisher( 
-                      PublisherID SERIAL PRIMARY KEY , 
-                      name VARCHAR(40), 
-                      adress VARCHAR(50),
-                      numberOfbooks INTEGER , 
-                      establishmentDate TIME ,
-                      logo VARBINARY(8000)
-                     );
-    CREATE TABLE Author( 
-                      AuthorID SERIAL PRIMARY KEY , 
-                      name VARCHAR(30), 
-                      surname VARCHAR(30),
-                      birthDate TIME , 
-                      numberOfbooks INTEGER,
-                      country VARCHAR(20)
-                     );
-                     
-    CREATE TABLE Books( 
-                      BookID SERIAL PRIMARY KEY , 
-                      Title VARCHAR(20),
-                      PageNum INTEGER,
-                      BookRewiev INTEGER REFERENCES BookRewiev (BookRewievID) , 
-                      PublisherID INTEGER REFERENCES Publisher (PublisherID), 
-                      AuthorID INTEGER REFERENCES Author (AuthorID)               
-                     );   
- 
-    CREATE TABLE BookRewiev( 
-                      BookRewievID SERIAL PRIMARY KEY , 
-                      UserID INTEGER REFERENCES Users (UserID), 
-                      BookID INTEGER REFERENCES Books  (BookID), 
-                      review INTEGER,
-                      PostDate TIME,
-                      UserRating INTEGER,
-                      lostEdit VARCHAR(50),
-                      orginalText VARCHAR(100) 
-                     ); 
+
                      
     INSERT INTO Users (name,surname, email,password,isAdmin) 
     VALUES ('admin','admin','admin@gmail.com', 'admin',1);
