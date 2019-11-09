677bd752e65e9c0cd9cfbece81f3e82c243d7dda
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-09 19:45:40 +0300

    Update dbinit.py
    Change table number 12 to 6

--- dbinit.py
+++ dbinit.py
@@ -6,53 +6,34 @@ import psycopg2 as dbapi2
 
 INIT_STATEMENTS = [
     """
-   CREATE TABLE Account( 
-                      AccountId serial PRIMARY KEY, 
-                      email VARCHAR (50) UNIQUE NOT NULL, 
-                      password VARCHAR (20) UNIQUE NOT NULL, 
-                      IsAdmin INTEGER 
-                     );
     CREATE TABLE Users( 
-                      UserID SERIAL PRIMARY KEY REFERENCES Account (AccountId), 
+                      UserID SERIAL PRIMARY KEY, 
                       name VARCHAR (50) UNIQUE NOT NULL, 
                       surname VARCHAR (50) UNIQUE NOT NULL, 
-                      gender VARCHAR (6) NULL , 
-                      age VARCHAR (3) NULL      
+                      gender VARCHAR (6) NULL, 
+                      age VARCHAR (3) NULL,
+                      content INTEGER REFERENCES UserContent (UserContentID),
+                      email VARCHAR (50) UNIQUE NOT NULL,
+                      password VARCHAR (20) UNIQUE NOT NULL,
+                      isAdmin INTEGER
                      );
-    CREATE TABLE Admin( 
-                      AdminID SERIAL PRIMARY KEY REFERENCES Account (AccountId), 
-                      HesapSahibi VARCHAR (60) UNIQUE NOT NULL, 
-                      HesapTipi VARCHAR (50) UNIQUE NOT NULL, 
-                      Iban VARCHAR (30) NULL , 
-                      Sube VARCHAR (30) NULL                 
-                     );  
-    CREATE TABLE CreditCard( 
-                      CreditCardID SERIAL PRIMARY KEY , 
-                      UserID INTEGER REFERENCES Users (UserID), 
-                      CrediCardNum VARCHAR (30) UNIQUE NULL, 
-                      CrediCardName VARCHAR (30) NULL , 
-                      CrediCardDate TIME                 
-                     );
-    CREATE TABLE AdminContent( 
-                      AdminContentID SERIAL PRIMARY KEY , 
-                      Title VARCHAR(30) UNIQUE NOT NULL  , 
-                      Price NUMERIC(4,2), 
-                      IssuaDate TIME , 
-                      PageNum INTEGER                
-                     );
+                     
     CREATE TABLE UserContent( 
                       UserContentID SERIAL PRIMARY KEY , 
                       Vote INTEGER, 
-                      VoteNum INTEGER, 
-                      Score FLOAT , 
-                      Comments VARCHAR(200)                
+                      VoteNum INTEGER,        
+                      Comment VARCHAR(200),
+                      FavBook INTEGER REFERENCES Books (BookID),
+                      FavAuthor INTEGER REFERENCES Author (AuthorID)
                      );
+                     
     CREATE TABLE Publisher( 
                       PublisherID SERIAL PRIMARY KEY , 
                       name VARCHAR(40), 
                       adress VARCHAR(50),
                       numberOfbooks INTEGER , 
-                      establishmentDate TIME               
+                      establishmentDate TIME ,
+                      logo VARBINARY(8000)
                      );
     CREATE TABLE Author( 
                       AuthorID SERIAL PRIMARY KEY , 
@@ -62,30 +43,16 @@ INIT_STATEMENTS = [
                       numberOfbooks INTEGER,
                       country VARCHAR(20)
                      );
+                     
     CREATE TABLE Books( 
                       BookID SERIAL PRIMARY KEY , 
-                      AdminContentID INTEGER REFERENCES AdminContent (AdminContentID) , 
-                      UserContentID INTEGER REFERENCES UserContent (UserContentID) , 
+                      Title VARCHAR(20),
+                      PageNum INTEGER,
+                      BookRewiev INTEGER REFERENCES BookRewiev (BookRewievID) , 
                       PublisherID INTEGER REFERENCES Publisher (PublisherID), 
                       AuthorID INTEGER REFERENCES Author (AuthorID)               
                      );   
-    CREATE TABLE Orders( 
-                      OrdersID SERIAL PRIMARY KEY , 
-                      UserID INTEGER REFERENCES Users (UserID), 
-                      BookID INTEGER REFERENCES Books  (BookID), 
-                      TotalAmount NUMERIC(4,2), 
-                      OrderStatusID INTEGER,
-                      OrderAdress INTEGER,
-                      BillingAdress INTEGER
-                     ); 
-                 
-    CREATE TABLE OrderStatus( 
-                      OrderStatusID SERIAL PRIMARY KEY REFERENCES Orders (OrdersID), 
-                      Statu VARCHAR(10), 
-                      CargoName VARCHAR(20), 
-                      TrackingNumber INTEGER, 
-                      SendTime TIME
-                     ); 
+ 
     CREATE TABLE BookRewiev( 
                       BookRewievID SERIAL PRIMARY KEY , 
                       UserID INTEGER REFERENCES Users (UserID), 
@@ -96,11 +63,8 @@ INIT_STATEMENTS = [
                       lostEdit VARCHAR(50),
                       orginalText VARCHAR(100) 
                      ); 
-    INSERT INTO Account (email, password, IsAdmin) 
-    VALUES ('example@gmail.com', '012345',0);
-    INSERT INTO Users (name, surname, gender, age) 
-    VALUES ('Example', 'example', 'male', 15);
-  
+    INSERT INTO Users (name,surname, email,password,isAdmin) 
+    VALUES ('admin','admin','admin@gmail.com', 'admin',1);
     """
 ]
 
