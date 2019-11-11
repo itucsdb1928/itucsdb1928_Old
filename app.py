a35db984d90e4260e4616b778ffb589cbdb90aaa
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-11 09:47:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 status=0 # for navigation bar view
 
-results = ["x","t"]
+my_list = ["x","t"]
 @app.route('/')
 @app.route('/Home')
 def homepage():
