18071375f33f69942c57305ff500935c03e74995
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-11 08:59:10 +0300

    Update scheme.sql

--- scheme.sql
+++ scheme.sql
@@ -1,15 +1,63 @@
 -------------------------------------------------------
 NEW
 
-CREATE TABLE Account( 
-                  AccountId serial PRIMARY KEY, 
-                  username VARCHAR (20) UNIQUE NOT NULL, 
-                  email VARCHAR (50) UNIQUE NOT NULL,
-                  phone VARCHAR (12) ,
-                  password VARCHAR (20) UNIQUE NOT NULL, 
-                  gender VARCHAR (6) NULL , 
-                 );
-                 
+    CREATE TABLE Author( 
+                      AuthorID SERIAL PRIMARY KEY , 
+                      name VARCHAR(30), 
+                      surname VARCHAR(30),
+                      birthDate TIME , 
+                      numberOfbooks INTEGER,
+                      country VARCHAR(20)
+                     );
+    CREATE TABLE BookRewiev( 
+                      BookRewievID SERIAL PRIMARY KEY ,
+                      review INTEGER,
+                      PostDate TIME,
+                      UserRating INTEGER,
+                      lostEdit VARCHAR(50),
+                      orginalText VARCHAR(100) 
+                     );
+                     
+    CREATE TABLE Publisher( 
+                      PublisherID SERIAL PRIMARY KEY , 
+                      name VARCHAR(40), 
+                      adress VARCHAR(50),
+                      numberOfbooks INTEGER , 
+                      establishmentDate TIME ,
+                      logo VARCHAR(50)
+                     );
+                     
+    CREATE TABLE Books( 
+                      BookID SERIAL PRIMARY KEY , 
+                      Title VARCHAR(20),
+                      PageNum INTEGER,
+                      BookRewiev INTEGER REFERENCES BookRewiev (BookRewievID) , 
+                      PublisherID INTEGER REFERENCES Publisher (PublisherID), 
+                      AuthorID INTEGER REFERENCES Author (AuthorID) ,
+                      cover VARCHAR(50)
+                     );  
+                    
+                                          
+    CREATE TABLE UserContent( 
+                      UserContentID SERIAL PRIMARY KEY , 
+                      Vote INTEGER, 
+                      VoteNum INTEGER,        
+                      Comment VARCHAR(200),
+                      FavBook INTEGER REFERENCES Books (BookID),
+                      FavAuthor INTEGER REFERENCES Author (AuthorID)
+                     );
+                     
+    CREATE TABLE Users( 
+                      UserID SERIAL PRIMARY KEY, 
+                      name VARCHAR (50) UNIQUE NOT NULL, 
+                      surname VARCHAR (50) UNIQUE NOT NULL, 
+                      gender VARCHAR (6) NULL, 
+                      age VARCHAR (3) NULL,
+                      content INTEGER REFERENCES UserContent (UserContentID),
+                      email VARCHAR (50) UNIQUE NOT NULL,
+                      password VARCHAR (20) UNIQUE NOT NULL,
+                      isAdmin INTEGER
+                     );
 ---------------------------------------------------------
 OLD
 
