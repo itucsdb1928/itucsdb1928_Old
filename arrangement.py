3d41c5d12b1e268cd0c27a2fb49a120f34b9b4ac
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-11 16:25:47 +0300

    Update arrangement.py

--- arrangement.py
+++ arrangement.py
@@ -4,9 +4,9 @@ import psycopg2.extras
 
 class Database:
 
-    def __init__(self, host="localhost",dbname="postgres", user="postgres", password="mrdfk1907"):
-        self.con = psycopg2.connect(database=dbname, user=user, password=password, host=host)
-        self.cur = self.con.cursor()
+ def __init__(self, dbname="d9qfricrmj8vii", user="tnivydbyztjmgh",	 
+                 password="b791cb0275732cebe4b23d3ce0d53d0d4a308966829b297983dafc39553f170a",	
+                 host="ec2-174-129-41-127.compute-1.amazonaws.com"):
 
     def get_home_page(self):
        with self.con.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
