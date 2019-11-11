3dfd0e5f57abc8d6e25a565afedbe9f294a7fb03
Muhammed Emin Topuz <muhammed-e.72@hotmail.com>
2019-11-11 16:54:14 +0300

    debug

--- arrangement.py
+++ arrangement.py
@@ -5,7 +5,10 @@ import psycopg2.extras
 class Database:
     def __init__(self, dbname="d9qfricrmj8vii", user="tnivydbyztjmgh",	 
                     password="b791cb0275732cebe4b23d3ce0d53d0d4a308966829b297983dafc39553f170a",	
-                    host="ec2-174-129-41-127.compute-1.amazonaws.com")
+                    host="ec2-174-129-41-127.compute-1.amazonaws.com"):
+        self.con = psycopg2.connect(database=dbname, user=user, password=password, host=host)
+        self.cur = self.con.cursor()
+    
     def get_home_page(self):
        with self.con.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
            query = "SELECT Books.Title,Author.name,Publisher.name FROM Books,Author,Publisher  WHERE Books.BookID=Publisher.PublisherID AND Books.BookID=Author.AuthorID"
