a9eb4b7bbaae10eba5295ae0bf9887ec8198143d
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-11 09:37:32 +0300

    Update arrangement.py

--- arrangement.py
+++ arrangement.py
@@ -4,9 +4,9 @@ import psycopg2.extras
 
 class Database:
 
-    def __init__(self, dbname="d9ai7lm2fcpac2", user="actbkeboyhuxmt",
-                 password="1fea78755cf3302bac2775a7084f213bde9769f6c1d416b9410f438158e584a3",
-                 host="ec2-174-129-252-252.compute-1.amazonaws.com"):
+    def __init__(self, dbname="d9qfricrmj8vii", user="tnivydbyztjmgh",
+                 password="b791cb0275732cebe4b23d3ce0d53d0d4a308966829b297983dafc39553f170a",
+                 host="ec2-174-129-41-127.compute-1.amazonaws.com"):
         self.con = psycopg2.connect(database=dbname, user=user, password=password, host=host)
         self.cur = self.con.cursor()
 
