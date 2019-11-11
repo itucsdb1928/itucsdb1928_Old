6f2c4b3f70fbee28770a3a1dbed730e68ddbb490
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-11 09:16:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from urllib.parse import urlparse
 import os
 import psycopg2 as dbapi2
-from arrangement import Database
+#from arrangement import Database
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -13,6 +13,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 #db=Database()
 
+"""
 status=0 # for navigation bar view 
 results = ["x","t"]
 @app.route('/')
@@ -62,4 +63,33 @@ def profile_page():
 if __name__ == '__main__':
     
     app.run(debug=True, use_reloader=True)
+"""
+@app.route('/')
+@app.route('/Home')
+def homepage():
+    global status
+    return render_template('home.html',books,Status =status)
 
+@app.route('/SignIn')
+def sign_in_page():
+    global status
+    status= 0
+    form = LoginForm()
+    return render_template('login.html',Status =status,title = "Login Page", form=form)
+
+@app.route('/SignUp')
+def sign_up_page():
+    global status
+    status= 0
+    return render_template('register.html',Status=status,title = "Register Page",form= form )
+
+@app.route('/Profile')
+def profile_page():
+    global status
+    status=1
+    return render_template('home.html',Status=status)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
