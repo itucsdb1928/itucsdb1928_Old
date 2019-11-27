89c59986637b383b0ef0a9732f176e752019039e
Muhammed Emin Topuz <topuzm15@gmail.com>
2019-11-27 18:01:35 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -6,12 +6,6 @@ import psycopg2 as dbapi2
 
 INIT_STATEMENTS = [
     """
-    DROP TABLE Author;
-    DROP TABLE BookRewiev;
-    DROP TABLE Publisher;
-    DROP TABLE Books;
-    DROP TABLE UserContent;
-    DROP TABLE Users;
     CREATE TABLE Author( 
                       AuthorID SERIAL PRIMARY KEY , 
                       name VARCHAR(30), 
